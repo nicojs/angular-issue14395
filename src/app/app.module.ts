625bcc3e578f8c81728dc3c07b06b69d0e1858e2
Nico Jansen <jansennico@gmail.com>
2017-02-10 15:05:38 +0100

    Add example in comment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Injectable } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule, ConnectionBackend, XHRBackend, Http } from '@angular/http';
+import { HttpModule, ConnectionBackend, XHRBackend, Http, RequestOptions } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
 @Injectable()
 export class HttpService extends Http {
+  // un-comment these lines and it works
+  // constructor(b: ConnectionBackend, r: RequestOptions) {
+  //   super(b, r);
+  // }
+
   blaat() {
     alert('Injected backend:' + this._backend);
   }
@@ -22,6 +27,7 @@ export class HttpService extends Http {
     HttpModule
   ],
   providers: [
+    { provide: ConnectionBackend, useClass: XHRBackend },
     { provide: Http, useClass: HttpService }
   ],
   bootstrap: [AppComponent]
